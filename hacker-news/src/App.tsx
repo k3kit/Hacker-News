a40928862ecd92703a70759890204383d80135de
k3kit <hadouken7809@gmail.com>
2022-11-14 23:47:14 +0400

    refactor: add react-router-v5.3.4, delete unused files

--- hacker-news/src/App.tsx
+++ hacker-news/src/App.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Home } from './pages/home/Home';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Link to="/">Home</Link>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
