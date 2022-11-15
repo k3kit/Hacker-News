8b2da2754923c70b983fee01d0d8032e6bf3f959
k3kit <hadouken7809@gmail.com>
2022-11-15 23:05:22 +0400

    fix: fix api function

--- hacker-news/src/utils/API/news.ts
+++ hacker-news/src/utils/API/news.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
-const API_URL = 'https://hacker-news.firebaseio.com/v0/';
+const API_URL = 'https://hacker-news.firebaseio.com/v0';
 const getNewsId = async () => {
-  const response = await axios.get(`${API_URL}/newsories.json`);
+  const response = await axios.get(`${API_URL}/newstories.json`);
   return response.data;
 };
 
-const gesNews = async (id: string) => {
+const getNews = async (id: number) => {
   const response = await axios.get(`${API_URL}item/${id}.json`);
   return response.data;
 };
 
-const getComment = async (id: string) => {
+const getComment = async (id: number) => {
   const response = await axios.get(`${API_URL}item/${id}.json`);
   return response.data;
 };
+
+export { getNewsId, getNews, getComment };
